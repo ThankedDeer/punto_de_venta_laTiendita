fbed9d5316cce3e3bb57343c096a1d5a8f642322
R1c4rdoPC <117680522+R1c4rdoPC@users.noreply.github.com>
2023-03-13 14:17:08 -0600

    Hola

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,29 +1,36 @@
 const items = document.getElementById('items')
-const templateCard = document.getElementById('template-card').content
+const templateCard = document.getElementById('template-card')
 const fragment = document.createDocumentFragment()
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
-})
-
 const fetchData = async () => {
-    try{
-        const res= await fetch('')
-        const data = await res.json()
-        console.log(data)
-        pintarCards(data)
-    }catch (error){
-        console.log(error)
-    }
+    axios.get('https://backtiendita-production.up.railway.app/api/vendedores')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 }
 
 const pintarCards = () => {
-    data.forEach(usuario => {
-        templateCard.querySelector('h1').textContent = usuario.nombre
+    data.forEach(vendedores => {
+        templateCard.querySelector('h1').textContent = vendedores.nombre
 
         const clone = template.cloneNode(true)
         fragment.appendChild(clone)
     });
 
     items.appendChild(fragment)
+}
+//Mostrar alerta--------------------------------------------------------------------------
+
+var textoInput = document.getElementById("recipient-name").value;
+var textoInput2 = document.getElementById("recipient-name2").value;
+
+function mostrarAlerta() {
+  if (textoInput.trim() && textoInput2.trim() !== "") {
+    console.log("El input tiene un valor");
+  } else {
+    console.log("El input no tiene un valor");
+  }
 }
