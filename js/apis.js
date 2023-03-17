db52ac5803428d2c92fca6506f20f3bd9f54e225
Yaelox <yaelsilvestre64@gmail.com>
2023-03-17 09:46:41 -0600

    Holas

--- js/apis.js
+++ js/apis.js
@@ -1,31 +1,14 @@
-const express = require ("express");
-const { verify } = require("jsonwebtoken");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const cors = require('cors');
-const app = express();
-
-app.post("/api/signin", async (req, res, next) => {
-    const reqData={};
-    var username = re.body.username;
-    var password = re.body.password;
-    
-    database.query('select * from vendedor where username=? and password=sha1(?)',[username,password],(err,rows,field)=>{
-        console.log(rows);
-        if(!err){
-            const hash=crypto.createHash('sha1').update(password).digest('hex');
-            if(rows.leghth == 1 && rows[0].username == username && rows[0].password == hash){
-                const user = rows[0];
-                jwt.sign({id: user.id}, config.secret,{expiresIn:"24h"},(err,token)=>{
-                    resp.json({token: token})
-                });
-            }
-            else{
-                resp.sendStatus(403);
-            }
-        }
-        else{
-            resp.sendStatus(503)
-        }
-    });
-});
+function login(){
+        credenciales ={};
+        credenciales.email = document.getElementById("email").value;
+        credenciales.password = document.getElementById("password").value;
+        axios.post("https://backtiendita-production.up.railway.app/api/usuarios", credenciales)
+        .then((response) => {
+            console.log(response.data.token);
+            sessionStorage.setItem("tToken", response.data.token);
+            window.location.href="./pages/menuPrincipal.html";
+        }).catch((error) => {
+            alert("error");
+            console.log(error);
+        });
+    }
