82358a81ae8f16370c57fe57ed36f36cc0526e3a
Yaelox <yaelsilvestre64@gmail.com>
2023-03-24 07:20:32 -0600

    Ponganse a chambear

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function login(){
+/* function login(){
     console.log("empieza funcion")
     credenciales = {};
     credenciales.Nom_Vendedor = document.getElementById("Nom_Vendedor").value;
@@ -13,5 +13,41 @@ function login(){
     })
     .catch((error) => {
         console.log(error);
+    });
+} */
+
+function login() {
+    console.log("empieza funcion");
+    var Nom_Vendedor = document.getElementById("Nom_Vendedor").value;
+    var Contraseña = document.getElementById("Contraseña").value;
+
+    if (!Nom_Vendedor || !Contraseña) {
+        console.log("Por favor, ingrese su nombre de usuario y contraseña.");
+        return;
+    }
+
+    var credenciales = {
+        Nom_Vendedor: Nom_Vendedor,
+        Contraseña: Contraseña
+    };
+
+    fetch('https://backtiendita-production-9419.up.railway.app/api/login', {
+        method: 'POST',
+        body: JSON.stringify(credenciales),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then((data) => {
+        window.location.href = "./pages/menuPrincipal.html";
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
     });
 }
