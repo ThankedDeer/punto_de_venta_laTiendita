34b0ac5aeef2fcf35c46a18830b387c69b5d17be
AbarrotesTiendita <abarroteslatiendita12@gmail.com>
2023-03-28 12:57:51 -0600

    pd

--- js/reportes.js
+++ js/reportes.js
@@ -2,21 +2,33 @@ const documentDefinition = {
   content: [
     { text: 'Corte de caja', style: 'header', alignment: 'center' },
     { text: 'Abarrotes (La tiendita)', style: 'header', alignment: 'center' },
-    { text: 'Carlos Sagredo, La Fundición, 20016 Aguascalientes, Ags', style: 'ubi', alignment: 'center' },
-    
-    {text: '-----------------'},
-    { text: 'Este es el contenido de mi documento PDF.', style: 'body' },
-    { text: 'Datos del servicio', style: 'subheader' },
-    { text: 'Esperando respuesta del servicio...', style: 'body' }
+
+    { text: 'Cajero: Administrador de la tienda', style: 'caja' },
+    { text: 'Carlos Sagredo, La Fundición, 20016 Aguascalientes, Ags', style: 'caja' },
+
+    {text: '==== VENTAS DEL DÍA ====', style: 'a1', alignment: 'center'},
+
+    { 
+      stack: [
+        { text: 'Datos de productos:', style: 'subheader' },
+        { text: 'producto1', text: '', style: 'body' } // Aquí se agregará el resultado de la API de productos
+      ]
+    },
+    { 
+      stack: [
+        { text: 'Datos de categorías:', style: 'subheader' },
+        { text: '', style: 'body' } // Aquí se agregará el resultado de la API de categorías
+      ]
+    }
   ],
   styles: {
     header: {
       fontSize: 18,
       bold: true,
-      margin: [0, 0, 0, 0]
+      margin: [0, 0, 0, 10]
     },
-    ubi: {
-      fontSize: 15,
+    caja: {
+      fontSize: 13,
       bold: false,
       margin: [0, 0, 0, 10]
     },
@@ -38,7 +50,7 @@ const promise1 = axios.get('https://backtiendita-production.up.railway.app/api/p
     res1.data.forEach(proveedor => {
       data1 += proveedor.Nom_Producto + '\n';
     });
-    documentDefinition.content.push({ text: data1 });
+    documentDefinition.content[5].stack[1].text = data1; // Se agrega el resultado en el segundo objeto 'text' del primer objeto 'stack'
   });
 
 const promise2 = axios.get('https://backtiendita-production.up.railway.app/api/categorias')
@@ -47,7 +59,7 @@ const promise2 = axios.get('https://backtiendita-production.up.railway.app/api/c
     res2.data.forEach(categoria => {
       data2 += categoria.Nom_Categoria + '\n';
     });
-    documentDefinition.content.push({ text: data2 });
+    documentDefinition.content[6].stack[1].text = data2; // Se agrega el resultado en el segundo objeto 'text' del segundo objeto 'stack'
   });
 
 Promise.all([promise1, promise2])
@@ -58,4 +70,3 @@ Promise.all([promise1, promise2])
   .catch(error => {
     console.error(error);
   });
-
