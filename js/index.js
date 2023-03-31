d59bb2dc1af597fd820c64e6b1ab56a01709fde1
Yaelox <yaelsilvestre64@gmail.com>
2023-03-30 18:13:49 -0600

    LOGEAR

--- js/index.js
+++ js/index.js
@@ -7,9 +7,10 @@ function login(){
     console.log(credenciales)
 
     axios
-    .post("https://backtiendita-production-9419.up.railway.app/api/login/"+ credenciales)
+    .post("http://localhost:3000/api/login/"+credenciales)
     .then((response) => {
-        console.log(response);
+        console.log(response.data.token);
+        sessionStorage.setItem("tToken", response.data.token);
         window.location.href = "./pages/menuPrincipal.html"; // Redirigir a otra página
     })
     .catch((error) => {
