f45c6932b4d5b592dc7432f6abb9f986ec284aef
Axel <axelolmar@gmail.com>
2023-04-05 21:23:51 -0600

    tratando de resolver categoria

--- js/categoria.js
+++ js/categoria.js
@@ -27,100 +27,107 @@ const nuevaCategoria = () => {
 };
 
 
-// function mostrarCategorias() {
-//   axios.get('http://localhost:3000/api/categorias')
-//     .then(response => {
-//       const categorias = response.data;
-//       const select = document.getElementById('actualizarCat');
-//       select.innerHTML = '<option selected value="">Selecciona la categoria</option>';
-//       categorias.forEach(categoria => {
-//         const option = document.createElement('option');
-//         option.value = categoria.idCategoria;
-//         option.dataset.nombre = categoria.Nom_Categoria;
-//         option.dataset.descripcion = categoria.Descripcion_Categoria;
-//         option.text = categoria.Nom_Categoria;
-//         select.appendChild(option);
-//       });
-//       select.addEventListener('change', mostrarDatosCategoria);
-//     })
-//     .catch(error => console.error(error));
+function mostrarCategorias() {
+  axios.get('http://localhost:3000/api/categorias')
+    .then(response => {
+      const categorias = response.data;
+      const select = document.getElementById('actualizarCat');
+      select.innerHTML = '<option selected value="">Selecciona la categoria</option>';
+      categorias.forEach(categoria => {
+        const option = document.createElement('option');
+        option.value = categoria.idCategoria;
+        option.dataset.nombre = categoria.Nom_Categoria;
+        option.dataset.descripcion = categoria.Descripcion_Categoria;
+        option.text = categoria.Nom_Categoria;
+        select.appendChild(option);
+      });
+      select.addEventListener('change', mostrarDatosCategoria);
+    })
+    .catch(error => console.error(error));
+}
+
+function mostrarDatosCategoria() {
+  const select = document.getElementById('actualizarCat');
+  const categoriaSeleccionada = select.options[select.selectedIndex];
+  if (categoriaSeleccionada !== null) {
+    const nombreCategoria = categoriaSeleccionada.dataset.nombre || "";
+    const descripcionCategoria = categoriaSeleccionada.dataset.descripcion || "";
+    document.getElementById("actualizarCategoria").value = nombreCategoria;
+    document.getElementById("actualizarDescripcion").value = descripcionCategoria;
+    console.log(nombreCategoria);
+    console.log(descripcionCategoria);
+  }
+}
+
+
+
+
+function editarCategoria() {
+  const select = document.getElementById('actualizarCat');
+  const categoriaSeleccionada = select.options[select.selectedIndex].value;
+  const nombre = document.getElementById("actualizarCategoria").value;
+  const descripcion = document.getElementById("actualizarDescripcion").value;
+
+  axios.patch(`http://localhost:3000/api/categoriact/${categoriaSeleccionada}`, { 
+      Nom_Categoria: nombre, 
+      Descripcion_Categoria: descripcion 
+    })
+    .then(response => {
+      console.log(response.data);
+      Swal.fire({
+        title: "Correcto",
+        text: "Categoria actualizada correctamente",
+        icon: "success",
+        confirmButtonText: "Cerrar",
+      });
+    })
+    .catch(error => {
+          console.error(error);
+          Swal.fire({
+            title: "Error al actualizar",
+            text: "Verifica bien los datos",
+            icon: "warning",
+            confirmButtonText: "Cerrar",
+          });
+        });
+      }
+
+mostrarCategorias();
+
+
+
+
+
+// function actualizarCategoria() {
+//   const nombreCategoria = document.getElementById("traerNombre").value;
+//   const nombreActualizado = document.getElementById("actualizarNombre").value;
+//   const descripcionActualizada = document.getElementById("actualizarDescripcion").value;
+
+//   axios.put(`http://localhost:3000/api/categoria/${nombreCategoria}`, {
+//     nombre: nombreActualizado,
+//     descripcion: descripcionActualizada
+//   })
+//   .then(response => {
+//     console.log(response.data);
+//     Swal.fire({
+//       title: "Correcto",
+//       text: "Categoria actualizada correctamente",
+//       icon: "success",
+//       confirmButtonText: "Cerrar",
+//     });
+//   })
+//   .catch(error => {
+//     console.error(error);
+//     Swal.fire({
+//       title: "Error al actualizar",
+//       text: "Verifica bien los datos",
+//       icon: "warning",
+//       confirmButtonText: "Cerrar",
+//     });
+//   });
 // }
 
-// function mostrarDatosCategoria() {
-//   const select = document.getElementById('actualizarCat');
-//   const categoriaSeleccionada = select.options[select.selectedIndex];
-//   document.getElementById("actualizarCategoria").value = categoriaSeleccionada.dataset.nombre;
-//   document.getElementById("actualizarDescripcion").value = categoriaSeleccionada.dataset.descripcion;
-//   console.log(categoriaSeleccionada);
-// }
-
-// function editarCategoria() {
-//   const select = document.getElementById('actualizarCat');
-//   const categoriaSeleccionada = select.options[select.selectedIndex].value;
-//   const nombre = document.getElementById("actualizarCategoria").value;
-//   const descripcion = document.getElementById("actualizarDescripcion").value;
-
-//   axios.put(`http://localhost:3000/api/categoria/${categoriaSeleccionada}`, { 
-//       Nom_Categoria: nombre, 
-//       Descripcion_Categoria: descripcion 
-//     })
-//     .then(response => {
-//       console.log(response.data);
-//       Swal.fire({
-//         title: "Correcto",
-//         text: "Categoria actualizada correctamente",
-//         icon: "success",
-//         confirmButtonText: "Cerrar",
-//       });
-//       location.reload();
-//     })
-//     .catch(error => {
-//           console.error(error);
-//           Swal.fire({
-//             title: "Error al actualizar",
-//             text: "Verifica bien los datos",
-//             icon: "warning",
-//             confirmButtonText: "Cerrar",
-//           });
-//         });
-//       }
-
-// mostrarCategorias();
-
-
-
-
-
-// // function actualizarCategoria() {
-// //   const nombreCategoria = document.getElementById("traerNombre").value;
-// //   const nombreActualizado = document.getElementById("actualizarNombre").value;
-// //   const descripcionActualizada = document.getElementById("actualizarDescripcion").value;
-
-// //   axios.put(`http://localhost:3000/api/categoria/${nombreCategoria}`, {
-// //     nombre: nombreActualizado,
-// //     descripcion: descripcionActualizada
-// //   })
-// //   .then(response => {
-// //     console.log(response.data);
-// //     Swal.fire({
-// //       title: "Correcto",
-// //       text: "Categoria actualizada correctamente",
-// //       icon: "success",
-// //       confirmButtonText: "Cerrar",
-// //     });
-// //   })
-// //   .catch(error => {
-// //     console.error(error);
-// //     Swal.fire({
-// //       title: "Error al actualizar",
-// //       text: "Verifica bien los datos",
-// //       icon: "warning",
-// //       confirmButtonText: "Cerrar",
-// //     });
-// //   });
-// // }
-
-// const habilitarCamposCategoria = function() {
-//   document.getElementById("actualizarCategoria").removeAttribute("readonly");
-//   document.getElementById("actualizarDescripcion").removeAttribute("readonly");
-// };
+const habilitarCamposCategoria = function() {
+  document.getElementById("actualizarCategoria").removeAttribute("readonly");
+  document.getElementById("actualizarDescripcion").removeAttribute("readonly");
+};
